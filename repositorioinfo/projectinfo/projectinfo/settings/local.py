f80303d92a8ef1ee8ec65c874fb83cd52162daee
Antovm002 <antonelavanesamaciel@gmail.com>
2022-07-30 18:06:48 -0300

    Initial commit

---
+++ repositorioinfo/projectinfo/projectinfo/settings/local.py
@@ -0,0 +1,19 @@
+from .settings import *
+
+SETTINGFILES_DIRS = {
+    os.path.join(os.path.dirname(BASE_DIR), "/calendarapp/eventcalendar/settings"),
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'mssql',
+        'NAME':'blogong4',
+        'USER': 'antitovm',
+        'PASSWORD': 'antito48962620',
+        'HOST': 'localhost\SQLEXPRESS',
+        'PORT': '',
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server'
+    }
+}
+}
