265d13221cb740b17ce13164207a5563bf9f52e5
Antovm002 <antonelavanesamaciel@gmail.com>
2022-08-04 17:32:18 -0300

    asd

--- repositorioinfo/projectinfo/projectinfo/settings/local.py
+++ repositorioinfo/projectinfo/projectinfo/settings/local.py
@@ -1,8 +1,7 @@
 from .settings import *
-import mysql.connector
 
-conexion = mysql.connector.connect(user='antonela', password='Antito48962620',host='localhost',database='blogong4',port='3306')
-"""SETTINGFILES_DIRS = {
+"""conexion = mysql.connector.connect(user='', password='Antito48962620',host='localhost',database='blogong4',port='3306')"""
+SETTINGFILES_DIRS = {
     os.path.join(os.path.dirname(BASE_DIR), "/calendarapp/eventcalendar/settings"),
 }
 
@@ -10,9 +9,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':'blogong4',
-        'USER': 'antitovm',
-        'PASSWORD': 'antito48962620',
+        'USER': 'root',
+        'PASSWORD': 'Antito48962620',
         'HOST': 'localhost',
-        'PORT': '',
+        'PORT': '3306',
 }
-}"""
+}
